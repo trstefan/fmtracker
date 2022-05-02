68cfff1f9dacd59892d2f0474cb9f70ba165896e
xStef23 <trstefy@gmail.com>
2022-05-02 13:13:46 +0100

    added script + some style

---
+++ script.js
@@ -0,0 +1,31 @@
+const projects = [
+  "3-col-cards",
+  "four-card-feature-section-master",
+  "sunnyside-agency-landing-page-main",
+  "testimonials-grid-section-main",
+];
+
+const list = document.getElementById("list-projects");
+
+projects.forEach((project, i) => {
+  const projectCard = document.createElement("li");
+  const projectLink = document.createElement("a");
+
+  const img = document.createElement("img");
+  img.src = `/${project}/design/desktop-design.jpg`;
+
+  projectLink.href = `/${project}/index.html`;
+  projectLink.innerText = `${i + 1}.${formatName(project)}`;
+  projectLink.prepend(img);
+  // projectCard.appendChild(img);
+  projectCard.appendChild(projectLink);
+
+  list.appendChild(projectCard);
+});
+
+function formatName(name) {
+  return name
+    .split("-")
+    .map((word) => word[0].toUpperCase() + word.slice(1))
+    .join(" ");
+}
